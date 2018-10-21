ce67e732cd968fde6437866df6ea22c8edf8c9db
Atrreydos <atrreydos@gmail.com>
2018-10-21 22:55:40 +0300

    add logging

--- src/main/java/ru/vigovskiy/strike_team/web/UserServlet.java
+++ src/main/java/ru/vigovskiy/strike_team/web/UserServlet.java
@@ -1,13 +1,23 @@
 package ru.vigovskiy.strike_team.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/users")
 public class UserServlet extends javax.servlet.http.HttpServlet {
+    
+    private static final Logger log = LoggerFactory.getLogger(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        log.debug("redirecting to users.jsp");
+
+        //        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("users.jsp");
     }
 }
