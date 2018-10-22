4de20da361e332dbafc6026b5acd2e2252949714
Atrreydos <atrreydos@gmail.com>
2018-10-22 11:05:51 +0300

    add Spring context

--- src/main/java/ru/vigovskiy/strike_team/service/UserServiceImpl.java
+++ src/main/java/ru/vigovskiy/strike_team/service/UserServiceImpl.java
@@ -1,14 +1,22 @@
 package ru.vigovskiy.strike_team.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.vigovskiy.strike_team.model.User;
 import ru.vigovskiy.strike_team.repository.UserRepository;
 import ru.vigovskiy.strike_team.util.exception.NotFoundException;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private UserRepository repository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public User get(int id) throws NotFoundException {
--- src/main/java/ru/vigovskiy/strike_team/web/rest/UserRestController.java
+++ src/main/java/ru/vigovskiy/strike_team/web/rest/UserRestController.java
@@ -2,15 +2,23 @@ package ru.vigovskiy.strike_team.web.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.vigovskiy.strike_team.model.User;
 import ru.vigovskiy.strike_team.service.UserService;
 
 import java.util.List;
 
-public class UserRestContoller {
-    private static final Logger log = LoggerFactory.getLogger(UserRestContoller.class);
-    
-    UserService service;
+@Controller
+public class UserRestController {
+    private static final Logger log = LoggerFactory.getLogger(UserRestController.class);
+
+    private UserService service;
+
+    @Autowired
+    public UserRestController(UserService service) {
+        this.service = service;
+    }
 
     public User get(int id) {
         log.info("get {}", id);
